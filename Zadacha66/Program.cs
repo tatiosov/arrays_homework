c02646d516e202f3ebd7bf498b3fdf7b48f7991c
tatiosov <141071546+tatiosov@users.noreply.github.com>
2023-09-19 16:25:02 +0300

    Added 64, 66, 68 solved tasks

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine($"Задача 66: Задайте значения M и N. Программа найдёт сумму натуральных элементов в промежутке от M до N");
+            int m = GetUserInput("Введите m: ");
+            int n = GetUserInput("Введите n: ");
+            int temp = m;
+
+            // Если m больше n, поменяем их значения местами.
+            if (m > n)
+            {
+                m = n;
+                n = temp;
+            }
+
+            CalculateAndPrintSum(m, n, temp = 0);
+
+            // Ожидание нажатия клавиши пользователем перед закрытием консоли.
+            Console.ReadKey();
+        }
+
+        // Функция для расчета и вывода суммы натуральных чисел от m до n.
+        static void CalculateAndPrintSum(int m, int n, int summ)
+        {
+            summ = summ + n;
+            
+            // Если n меньше или равно m, выводим сумму элементов.
+            if (n <= m)
+            {
+                Console.Write($"Сумма элементов = {summ} ");
+                return;
+            }
+
+            // Рекурсивный вызов функции для расчета суммы с уменьшением n на 1.
+            CalculateAndPrintSum(m, n - 1, summ);
+        }
+
+        // Функция для получения целого числа от пользователя.
+        static int GetUserInput(string input)
+        {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+        }
+    }
+}
---
+++ Zadacha68/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine($"Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+            int m = GetUserInput("Введите m: ");
+            int n = GetUserInput("Введите n: ");
+
+            int ackermanFunction = CalculateAckerman(m, n);
+
+            Console.Write($"Функция Аккермана = {ackermanFunction} ");
+
+            // Ждем, пока пользователь нажмет клавишу, чтобы консоль не закрылась сразу
+            Console.ReadKey();
+        }
+
+        // Рекурсивная функция для вычисления функции Аккермана.
+        static int CalculateAckerman(int m, int n)
+        {
+            if (m == 0) return n + 1;
+            else if (n == 0) return CalculateAckerman(m - 1, 1);
+            else return CalculateAckerman(m - 1, CalculateAckerman(m, n - 1));
+        }
+
+        // Функция для получения целого числа от пользователя.
+        static int GetUserInput(string input)
+        {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            
+            // Если введено отрицательное число, преобразуем его в положительное и выведем информацию о модификации.
+            if (output < 0)
+            {
+                Console.WriteLine($"По заданию число не может быть отрицательным, получаем модуль заданного вами числа {output}");
+                output = Math.Abs(output);
+                Console.WriteLine($"Новое число: {output}");
+            }
+            return output;
+        }
+    }
+}
